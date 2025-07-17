137d3df8f87f15c6288e0354cafc810213396b6c
yothgewalt <eine.schlacht@gmail.com>
2025-07-17 19:24:37 +0700

    Fix age classification logic to use correct conditions for age ranges

--- week1/conditionals/ages.py
+++ week1/conditionals/ages.py
@@ -1,10 +1,10 @@
 age = int(input("age > "))
 
-if age > 0:
+if age > 0 and age < 13:
     print("เด็ก")
-elif age >= 13 or age <= 19:
+elif age >= 13 and age <= 19:
     print("วัยรุ่น")
-elif age >= 20 or age <= 59:
+elif age >= 20 and age <= 59:
     print("ผู้ใหญ่")
 elif age >= 60 :
     print("ผู้สูงอายุ")
